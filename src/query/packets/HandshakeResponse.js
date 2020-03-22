eeb1b77e096686fbe17e528ee826546f7a40c55f
SharletP <sharletman@gmail.com>
2020-03-22 20:43:44 +0300

    Added HandshakeResponse Packet.

---
+++ src/query/packets/HandshakeResponse.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const Packet = require("./Packet.js");
+
+class HandshakeResponse extends Packet {
+  constructor() {
+    super();
+    
+    this.sessionID;
+    this.payload;
+  }
+  
+  decodeHeader() {
+    this.readByte();
+    this.sessionID = this.readInt();
+  }
+  
+  decodePayload() {
+    this.payload = this.readLong();
+  }
+};
+
+module.exports = HandshakeResponse;
