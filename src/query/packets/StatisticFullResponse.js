bac179d7a108dd4686122c08ab38e877302d91e2
SharletP <sharletman@gmail.com>
2020-03-23 01:41:41 +0300

    Added StatisticsFullResponse Packet, note (check if plugins k, v parses).

---
+++ src/query/packets/StatisticFullResponse.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const Packet = require("./Packet.js");
+
+class StatisticFullResponse extends Packet {
+  constructor() {
+    super();
+    
+    this.sessionID;
+    this.payload;
+  }
+  
+  decodeHeader() {
+    this.readByte();
+    this.sessionID = this.readInt();
+  }
+  
+  decodePayload() {
+    this.payload = {};
+    
+    this.readData(11);
+    
+    this.payload.hostname = this.parseString();
+    this.payload.gametype = this.parseString();
+    this.payload.gameID = this.parseString();
+    this.payload.version = this.parseString();
+    this.payload.plugins = this.parseString();
+    this.payload.map = this.parseString();
+    this.payload.numPlayers = this.parseString();
+    this.payload.maxPlayers = this.parseString();
+    this.payload.hostPort = this.readLShort();
+    this.payload.hostIP = this.parseString();
+    
+    this.readData(10);
+    
+    this.payload.players = [];
+    for (let i = this.getOffset(); i < this.length; i = this.getOffset()) {
+      this.payload.players.push(this.parseString());
+    };
+  }
+};
+
+module.exports = StatisticFullResponse;
