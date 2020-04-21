5cf62ce9fe23acf9ea9a874c37c912d8b4c9262f
SharletP <sharletman@gmail.com>
2020-04-21 21:48:21 +0300

    Updated SharletPQueryRcon.js

--- src/SharletPQueryRcon.js
+++ src/SharletPQueryRcon.js
@@ -2,9 +2,15 @@
 
 const Console = require("./console/Console.js");
 
+const Query = require("./query/Query.js");
+const RCON = require("./rcon/RCON.js");
+
 class SharletPQueryRcon {
   constructor() {
     this.console = new Console();
+    
+    this.QueryClient = new Query(this.console);
+    this.RconClient = new RCON(this.console);
   }
 };
 
