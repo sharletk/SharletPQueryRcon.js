9e74dc092ea842c0fd4b9071d9f82e2513de5bb0
sharletp <sharletman@gmail.com>
2020-09-26 23:18:07 +0300

    Added linting; Added Github Actions;

---
+++ .eslintrc.js
@@ -0,0 +1,61 @@
+module.exports = {
+  "env": {
+    "commonjs": true,
+    "es6": true,
+    "node": true
+  },
+  "extends": "eslint:recommended",
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaVersion": 2018
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2,
+      {
+        "SwitchCase": 1
+      }
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "keyword-spacing": [
+      "error", {
+        "before": true,
+        "after": true
+      }
+    ],
+    "space-before-blocks": [
+      "error", {
+        "functions":"always",
+        "keywords": "always",
+        "classes": "always"
+      }
+    ],
+    "space-before-function-paren": [
+      "error", {
+        "anonymous": "never",
+        "named": "never",
+        "asyncArrow": "always"
+      }
+    ],
+    "no-useless-escape": "off",
+    "no-undef": "off",
+    "no-case-declarations": "off",
+    "no-unused-vars": "off",
+    "no-unreachable": "off"
+  }
+};
